e738480940adf4ffb375bf4c3b4794c1473d2524
Chongyi Zheng <git@zcy.dev>
2023-09-20 21:32:35 -0400

    Install Node.js (#8)

---
+++ images/ubuntu/scripts/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get upgrade -y
+apt-get install -y curl tar xz-utils
+
+archstr=$(uname -m)
+if [[ "$archstr" == "x86_64" ]]; then
+  arch="x64"
+elif [[ "$archstr" == "arm64" || "$archstr" == "arm" || "$archstr" == "aarch64" ]]; then
+  arch="arm64"
+else
+  echo "Unsupported architecture: $archstr"
+  return 1
+fi
+
+NODE_VERSION="20.7.0"
+curl https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-$arch.tar.xz | tar --file=- --extract --xz --directory /usr/local/ --strip-components=1
