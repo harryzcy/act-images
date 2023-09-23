812d93867051cec727193a6e20bacb6529fbb4e0
Chongyi Zheng <git@zcy.dev>
2023-09-23 02:32:45 -0400

    Fix go download

--- images/ubuntu/scripts/install.sh
+++ images/ubuntu/scripts/install.sh
@@ -56,7 +56,7 @@ else
   return 1
 fi
 
-tar -C /usr/local -xzf go$GO_VERSION.linux-$go_arch.tar.gz
+curl https://go.dev/dl/go$GO_VERSION.linux-$go_arch.tar.gz | tar --file=- -C /usr/local -xzf go$GO_VERSION.linux-$go_arch.tar.gz
 # End Go
 
 # Docker
