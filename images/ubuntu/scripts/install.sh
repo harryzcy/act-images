195594554c0f7af1f4731ac74c8a50297a39d054
Chongyi Zheng <git@zcy.dev>
2023-09-25 16:39:53 -0400

    Include dependencies from buildpack-deps (#26)

--- images/ubuntu/scripts/install.sh
+++ images/ubuntu/scripts/install.sh
@@ -8,6 +8,48 @@ apt-get -qq update
 apt-get -qq -y upgrade
 
 packages=(
+  # buildpack packages
+  autoconf
+  automake
+  bzip2
+  dpkg-dev
+  file
+  g++
+  gcc
+  imagemagick
+  libbz2-dev
+  libc6-dev
+  libcurl4-openssl-dev
+  libdb-dev
+  libevent-dev
+  libffi-dev
+  libgdbm-dev
+  libglib2.0-dev
+  libgmp-dev
+  libjpeg-dev
+  libkrb5-dev
+  liblzma-dev
+  libmagickcore-dev
+  libmagickwand-dev
+  libmaxminddb-dev
+  libncurses5-dev
+  libncursesw5-dev
+  libpng-dev
+  libpq-dev
+  libreadline-dev
+  libsqlite3-dev
+  libssl-dev
+  libtool
+  libwebp-dev
+  libxml2-dev
+  libxslt-dev
+  libyaml-dev
+  make
+  patch
+  unzip
+  xz-utils
+  zlib1g-dev
+  # end buildpack
   ssh
   gawk
   curl
@@ -22,8 +64,6 @@ packages=(
   libyaml-0-2
   zstd
   zip
-  unzip
-  xz-utils
   sqlite3
   python3-pip
   python3-venv
