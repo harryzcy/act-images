6e1438bb9f899c9aed3b06d5e5599a5a5690fdc2
Chongyi Zheng <git@zcy.dev>
2023-09-26 17:45:57 -0400

    Print more packages' versions

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-echo "$(git --version) $(which git)"
-echo "$(docker -v) $(which docker)"
+# Languages
 echo "node $(node -v) $(which node)"
-echo "$(python --version) $(which python)"
 echo "$(go version) $(which go)"
+echo "$(python --version) $(which python)"
 echo "$(rustc --version) $(which rustc)"
+
+# Package Managers
+echo "$(cargo --version) $(which cargo)"
 echo "npm $(npm -v) $(which npm)"
+echo "$(pip -V) $(which pip)"
+echo "$(pipx --version) $(which pipx)"
+
+# Tools
 ansible --version
 ansible-lint --version
+awk --version
+curl --version
+echo "$(docker -v) $(which docker)"
+echo "$(git --version) $(which git)"
+echo "$(sqlite3 --version) $(which sqlite3)"
+jq --version
+wget --version
+zip --version
