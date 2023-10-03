fbc34879ecb31106f076b488d40167f590303790
Chongyi Zheng <git@zcy.dev>
2023-10-02 22:02:14 -0400

    Use multi-stage build to shrink image size (#42)

---
+++ images/ubuntu/scripts/build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+PYTHON_VERSION="3.11.5"
+
+GIT_VERSION="2.42.0"
+
+packages=(
+  ca-certificates
+  curl
+  # required by python
+  build-essential
+  libbz2-dev
+  libffi-dev
+  libgdbm-dev
+  libncurses5-dev
+  libsqlite3-dev
+  libssl-dev
+  # required by git
+  install-info
+  dh-autoreconf
+  libexpat1-dev
+  gettext
+  libz-dev
+  # end
+)
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get -qq update
+apt-get -qq -y upgrade
+apt-get -qq -y install --no-install-recommends --no-install-suggests "${packages[@]}"
+
+# git (build from source)
+echo "Installing git"
+pushd /tmp || exit >/dev/null
+curl -OL https://mirrors.edge.kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.gz
+tar -xzvf git-$GIT_VERSION.tar.gz >/dev/null
+pushd git-$GIT_VERSION/ || exit
+make -j "$(nproc)" configure
+./configure --prefix=/usr/local
+make -j "$(nproc)" all
+make -j "$(nproc)" install
+popd || exit
+popd || exit
+rm -rf /tmp/git-$GIT_VERSION
+echo "git installed: $(git --version)"
+# End git
+
+# Python
+echo "Installing Python $PYTHON_VERSION"
+pushd /tmp || exit >/dev/null
+curl -sSOL https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
+tar -xzvf Python-$PYTHON_VERSION.tgz >/dev/null
+pushd Python-$PYTHON_VERSION/ || exit
+
+./configure --enable-optimizations >/dev/null
+make -j "$(nproc)" && make -j "$(nproc)" altinstall
+
+ln -s /usr/local/bin/python3.11 /usr/local/bin/python
+ln -s /usr/local/bin/python3.11 /usr/local/bin/python3
+ln -s /usr/local/bin/pip3.11 /usr/local/bin/pip
+ln -s /usr/local/bin/pip3.11 /usr/local/bin/pip3
+
+popd || exit
+popd || exit
+rm -rf /tmp/Python-$PYTHON_VERSION
+echo "Python installed: $(python --version) at $(which python)"
+# End Python
--- images/ubuntu/scripts/install.sh
+++ images/ubuntu/scripts/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+set -o errexit
+set -o pipefail
+
 NODE_VERSION="20.7.0"
 GO_VERSION="1.21.1"
-PYTHON_VERSION="3.11.5"
 
-GIT_VERSION="2.42.0"
 JQ_VERSION="1.7"
 
 archstr=$(uname -m)
@@ -65,50 +66,27 @@ packages=(
   xz-utils
   zlib1g-dev
   # end buildpack
-  # required for building git
-  install-info
-  dh-autoreconf
-  libexpat1-dev
-  gettext
-  libz-dev
-  # end
-  ssh
-  gawk
+  apt-transport-https
+  build-essential
+  ca-certificates
   curl
-  wget
+  libyaml-0-2
   sudo
+  gawk
   gnupg
   gnupg-agent
-  ca-certificates
+  ssh
   software-properties-common
-  apt-transport-https
-  libyaml-0-2
+  wget
   zstd
   zip
   sqlite3
-  build-essential
 )
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get -qq update
 apt-get -qq -y upgrade
 apt-get -qq -y install --no-install-recommends --no-install-suggests "${packages[@]}"
-
-# git (build from source)
-echo "Installing git"
-pushd /tmp || exit >/dev/null
-curl -OL https://mirrors.edge.kernel.org/pub/software/scm/git/git-$GIT_VERSION.tar.gz
-tar -xzvf git-$GIT_VERSION.tar.gz >/dev/null
-pushd git-$GIT_VERSION/ || exit
-make configure
-./configure --prefix=/usr/local
-make all
-sudo make install
-popd || exit
-popd || exit
-rm -rf /tmp/git-$GIT_VERSION
-echo "git installed: $(git --version)"
-# End git
 
 # Docker
 echo "Installing Docker"
@@ -127,29 +105,11 @@ rm /etc/apt/keyrings/docker.gpg
 echo "Docker installed: $(docker --version)"
 # End Docker
 
-# Python
-echo "Installing Python $PYTHON_VERSION"
-pushd /tmp || exit >/dev/null
-wget -q https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
-tar -xzvf Python-$PYTHON_VERSION.tgz >/dev/null
-pushd Python-$PYTHON_VERSION/ || exit
-
-./configure --enable-optimizations >/dev/null
-make -j "$(nproc)" && make -j "$(nproc)" altinstall
-
-ln -s /usr/local/bin/python3.11 /usr/local/bin/python
-ln -s /usr/local/bin/python3.11 /usr/local/bin/python3
-ln -s /usr/local/bin/pip3.11 /usr/local/bin/pip
-ln -s /usr/local/bin/pip3.11 /usr/local/bin/pip3
-
-python -m pip install --user pipx
-python -m pipx ensurepath
-
-popd || exit
-popd || exit
-rm -rf /tmp/Python-$PYTHON_VERSION
-echo "Python installed: $(python --version) at $(which python)"
-# End Python
+# Go
+echo "Installing Go $GO_VERSION"
+curl -L https://go.dev/dl/go$GO_VERSION.linux-$arch.tar.gz | tar -C /usr/local -xzf -
+echo "Go installed: $(go version) at $(which go)"
+# End Go
 
 # Node
 echo "Installing Node $NODE_VERSION"
@@ -157,17 +117,16 @@ curl https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-$arch_shor
 echo "Node installed: $(node -v) at $(which node)"
 # End Node
 
-# Go
-echo "Installing Go $GO_VERSION"
-curl -L https://go.dev/dl/go$GO_VERSION.linux-$arch.tar.gz | tar -C /usr/local -xzf -
-echo "Go installed: $(go version) at $(which go)"
-# End Go
-
 # Rust
 echo "Installing Rust"
 curl -fsSL https://sh.rustup.rs | sh -s -- -y --default-toolchain=stable --profile=minimal
 echo "Rust installed: $(rustc --version) at $(which rustc)"
 # End Rust
+
+# Python
+python -m pip install --user pipx
+python -m pipx ensurepath
+# End Python
 
 # Ansible
 echo "Installing Ansible"
@@ -181,4 +140,5 @@ echo "Ansible Lint installed: $(ansible-lint --version) at $(which ansible-lint)
 echo "Installing jq"
 curl -L https://github.com/jqlang/jq/releases/download/jq-$JQ_VERSION/jq-linux-$arch -o /usr/local/bin/jq
 chmod +x /usr/local/bin/jq
+echo "jq installed: $(jq --version) at $(which jq)"
 # End jq
