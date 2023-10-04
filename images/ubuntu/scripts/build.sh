d10f7ffe0f5cdfd5f56eaf269f4881b53ae65b13
Chongyi Zheng <git@zcy.dev>
2023-10-04 04:24:18 -0400

    Provide optional dependencies for building python

--- images/ubuntu/scripts/build.sh
+++ images/ubuntu/scripts/build.sh
@@ -13,12 +13,23 @@ packages=(
   curl
   # required by python
   build-essential
+  gdb
+  lcov
+  pkg-config
   libbz2-dev
   libffi-dev
   libgdbm-dev
+  libgdbm-compat-dev
+  liblzma-dev
   libncurses5-dev
+  libreadline6-dev
   libsqlite3-dev
   libssl-dev
+  lzma
+  lzma-dev
+  tk-dev
+  uuid-dev
+  zlib1g-dev
   # required by git
   install-info
   dh-autoreconf
