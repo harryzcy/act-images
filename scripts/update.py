18aa5b46d64b5066f8b509fd7287bf1b0bece626
Chongyi Zheng <git@zcy.dev>
2023-11-17 08:28:20 -0500

    Add script for checking updates

---
+++ scripts/update.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+from urllib.request import urlopen
+import re
+import json
+
+
+def read_readme():
+    with open("README.md") as f:
+        readme = f.read()
+    return readme
+
+
+def get_current_version(readme: str, language: str):
+    pattern = r"{} ([0-9.]+)".format(language)
+    current = re.search(pattern, readme)[1]
+    return current
+
+
+def check_go_version(readme: str):
+    url = "https://golang.org/VERSION?m=text"
+    with urlopen(url) as f:
+        latest = f.read().decode("utf-8").strip()
+    latest = latest.split("\n")[0]
+    latest = latest.removeprefix("go")
+    current = get_current_version(readme, "Go")
+    return current != latest, current, latest
+
+
+def check_node_version(readme: str):
+    url = "https://nodejs.org/download/release/index.json"
+    with urlopen(url) as f:
+        releases = json.loads(f.read().decode("utf-8").strip())
+
+    for release in releases:
+        if release["lts"]:
+            latest = release["version"]
+            break
+    latest = latest.removeprefix("v")
+    current = get_current_version(readme, "Node")
+    return current != latest, current, latest
+
+
+def check_python_version(readme: str):
+    url = "https://endoflife.date/api/python.json"
+    with urlopen(url) as f:
+        releases = json.loads(f.read().decode("utf-8").strip())
+    latest = releases[0]["latest"]
+    current = get_current_version(readme, "Python")
+    return current != latest, current, latest
+
+
+def main():
+    readme = read_readme()
+
+    print(check_go_version(readme))
+    print(check_node_version(readme))
+    print(check_python_version(readme))
+
+
+if __name__ == "__main__":
+    main()
