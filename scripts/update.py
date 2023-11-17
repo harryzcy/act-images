d165da377ed23ef3cb3851daf2a5a329f7873f88
Chongyi Zheng <git@zcy.dev>
2023-11-17 08:39:18 -0500

    Print result

--- scripts/update.py
+++ scripts/update.py
@@ -15,12 +15,13 @@ def get_current_version(readme: str, language: str):
     pattern = r"{} ([0-9.]+)".format(language)
     matches = re.search(pattern, readme)
     if matches is None:
-        raise Exception("Could not find {} version".format(language))
+        print("Could not find {} version in README.md".format(language))
+        return None
     current = matches[1]
     return current
 
 
-def check_go_version(readme: str):
+def check_go(readme: str):
     url = "https://golang.org/VERSION?m=text"
     with urlopen(url) as f:
         latest = f.read().decode("utf-8").strip()
@@ -30,7 +31,7 @@ def check_go_version(readme: str):
     return current != latest, current, latest
 
 
-def check_node_version(readme: str):
+def check_node(readme: str):
     url = "https://nodejs.org/download/release/index.json"
     with urlopen(url) as f:
         releases = json.loads(f.read().decode("utf-8").strip())
@@ -44,7 +45,7 @@ def check_node_version(readme: str):
     return current != latest, current, latest
 
 
-def check_python_version(readme: str):
+def check_python(readme: str):
     url = "https://endoflife.date/api/python.json"
     with urlopen(url) as f:
         releases = json.loads(f.read().decode("utf-8").strip())
@@ -68,13 +69,40 @@ def check_git(readme: str):
     return current != latest, current, latest
 
 
+def check_jq(readme: str):
+    url = "https://api.github.com/repos/jqlang/jq/tags"
+    with urlopen(url) as f:
+        tags = json.loads(f.read().decode("utf-8").strip())
+
+    for tag in tags:
+        name = tag["name"]
+        if "rc" not in name:
+            latest = name
+            break
+    latest = latest.removeprefix("jq-")
+    current = get_current_version(readme, "jq")
+    return current != latest, current, latest
+
+
 def main():
     readme = read_readme()
 
-    print(check_go_version(readme))
-    print(check_node_version(readme))
-    print(check_python_version(readme))
-    print(check_git(readme))
+    checks = {
+        "Go": check_go,
+        "Node": check_node,
+        "Python": check_python,
+        "git": check_git,
+        "jq": check_jq,
+    }
+
+    num_updates = 0
+    for package, check in checks.items():
+        update, current, latest = check(readme)
+        if update:
+            num_updates += 1
+            print(f"{package}: {current} -> {latest}")
+    if num_updates == 0:
+        print("No updates available")
 
 
 if __name__ == "__main__":
