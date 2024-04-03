7976c2fe1d933eca9245c9918cd4f5009db64b4e
Chongyi Zheng <git@zcy.dev>
2024-04-03 18:03:50 -0400

    Pin pipx by sha256

--- scripts/update.py
+++ scripts/update.py
@@ -21,7 +21,9 @@ def write_packages(packages: dict):
         f.write("\n")
 
 
-def update_environment(package: str, from_version: str, to_version: str):
+def update_environment(
+    package: str, from_version: str, to_version: str, sha256s: list = None
+):
     package_env = f"{package.upper().replace('-', '_')}_VERSION"
     old_env = f'{package_env}="{from_version}"'
     new_env = f'{package_env}="{to_version}"'
@@ -31,19 +33,45 @@ def update_environment(package: str, from_version: str, to_version: str):
         new_content = contents.replace(old_env, new_env)
         if new_content != contents:
             print(f"Updating {file}")
+
+            if sha256s:
+                # Update the sha256s
+                lines = new_content.split("\n")
+                line_number = 0
+                for line in lines:
+                    line_number += 1
+                    if f"${package_env}" in line and "pip install" in line:
+                        break
+                if not line.strip().endswith("\\"):
+                    line[line_number] = line.strip() + " \\"
+                    line_number += 1
+                # remove the old sha256s
+                while "sha256:" in lines[line_number]:
+                    lines.pop(line_number)
+                # add the new sha256s
+                for index, sha256 in enumerate(sha256s):
+                    lines.insert(
+                        line_number,
+                        f"  --hash=sha256:{sha256}"
+                        if index == len(sha256s) - 1
+                        else f"  --hash=sha256:{sha256} \\",
+                    )
+                    line_number += 1
+                new_content = "\n".join(lines)
+
             with open(file, "w") as f:
                 f.write(new_content)
         else:
             print(f"Skipping {file}")
 
 
-def update_current(packages: dict, package: str, version: str):
+def update_current(packages: dict, package: str, version: str, sha256s: list = None):
     for group in packages:
         for item in group["items"]:
             if item["name"] == package:
                 if item["version"] == version:
                     return False
-                update_environment(package, item["version"], version)
+                update_environment(package, item["version"], version, sha256s)
                 item["version"] = version
                 return True
     return None
@@ -93,6 +121,17 @@ def get_version_from_tag(owner: str, repo: str, prefix: str = "v"):
         if prefix == "" and name[0].isdigit():
             return name
     return None
+
+
+def get_version_from_pypi(project: str):
+    url = f"https://pypi.org/pypi/{project}/json"
+    with urlopen(url) as f:
+        content = json.loads(f.read().decode("utf-8").strip())
+    version = content["info"]["version"]
+    sha256s = []
+    for release in content["releases"][version]:
+        sha256s.append(release["digests"]["sha256"])
+    return version, sha256s
 
 
 def get_version_from_release(owner: str, repo: str, prefix: str = "v"):
@@ -162,8 +201,8 @@ def update_jq(packages: dict):
 
 
 def update_pipx(packages: dict):
-    latest = get_version_from_release("pypa", "pipx")
-    return update_current(packages, "pipx", latest)
+    latest, sha256s = get_version_from_pypi("pipx")
+    return update_current(packages, "pipx", latest, sha256s)
 
 
 def update_typos_cli(packages: dict):
@@ -190,23 +229,23 @@ def main():
     packages = get_packages()
 
     checks = {
-        "Go": update_go,
-        "Node": update_node,
-        "Python": update_python,
-        "Rust": update_rust,
-        "pip": update_pip,
+        # "Go": update_go,
+        # "Node": update_node,
+        # "Python": update_python,
+        # "Rust": update_rust,
+        # "pip": update_pip,
         "pipx": update_pipx,
-        "git": update_git,
-        "ansible": update_ansible,
-        "ansible-core": update_ansible_core,
-        "ansible-lint": update_ansible_lint,
-        "kubeconform": update_kubeconform,
-        "kube-linter": update_kube_linter,
-        "jq": update_jq,
-        "typos-cli": update_typos_cli,
-        "ruff": update_ruff,
-        "rustup": update_rustup,
-        "yamllint": update_yamllint,
+        # "git": update_git,
+        # "ansible": update_ansible,
+        # "ansible-core": update_ansible_core,
+        # "ansible-lint": update_ansible_lint,
+        # "kubeconform": update_kubeconform,
+        # "kube-linter": update_kube_linter,
+        # "jq": update_jq,
+        # "typos-cli": update_typos_cli,
+        # "ruff": update_ruff,
+        # "rustup": update_rustup,
+        # "yamllint": update_yamllint,
     }
 
     num_updates = 0
