6c0266bb432452658cc7718d4a42d7e1e93f58d8
Chongyi Zheng <git@zcy.dev>
2024-04-13 22:25:22 -0400

    Skip alpha/beta tag indicated by letter (#160)

--- scripts/update.py
+++ scripts/update.py
@@ -143,7 +143,7 @@ def get_version_from_tag(owner: str, repo: str, prefix: str = "v"):
 
     for tag in tags:
         name: str = tag["name"]
-        if "rc" in name:
+        if "rc" in name or "a" in name or "b" in name:
             continue
         if name.startswith(prefix):
             return name.removeprefix(prefix)
