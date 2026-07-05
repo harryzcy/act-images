5fafbf957ef1a257c96d59100ce5b7f066517efa
Chongyi Zheng <git@zcy.dev>
2026-07-04 21:34:15 -0500

    Install rust before git

--- images/ubuntu/24.04/scripts/build.sh
+++ images/ubuntu/24.04/scripts/build.sh
@@ -7,6 +7,7 @@ echo "Loading package versions for build"
 source /versions.env
 
 echo "PYTHON_VERSION: $PYTHON_VERSION"
+echo "RUST_VERSION: $RUST_VERSION"
 
 echo "PIP_VERSION: $PIP_VERSION"
 echo "GIT_VERSION: $GIT_VERSION"
@@ -53,6 +54,12 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -qq update
 apt-get -qq -y upgrade
 apt-get -qq -y install --no-install-recommends --no-install-suggests "${packages[@]}"
+
+# Rust
+echo "Installing Rust"
+curl -fsSL https://raw.githubusercontent.com/rust-lang/rustup/$RUSTUP_VERSION/rustup-init.sh | sh -s -- -y --default-toolchain=$RUST_VERSION --profile=minimal
+echo "Rust installed: $(rustc --version) at $(which rustc)"
+# End Rust
 
 # git (build from source)
 echo "Installing git"
